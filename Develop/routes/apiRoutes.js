fe451f7efe981e99debd983730eb28b16bac1918
Mary Wood <mwoodervin@gmail.com>
2020-09-02 15:26:44 -0400

    Notes displaying on page!

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,32 +1,58 @@
 
 const fs = require("fs");
 
-// accessing the data in the db.json file
-let rawNotesData = fs.readFileSync("./db/db.json");
-let notesData = JSON.parse(rawNotesData);
-// let notes = [...notesData];
-// console.log(notesData);
-// console.log(...notesData);
-console.log(notesData);
-
 // Routing
-module.exports = function(app) {
-    app.get("/api/notes", function(req, res) {
-        res.send(notesData);
+module.exports = function (app) {
+    app.get("/api/notes", function (req, res) {
+        fs.readFile('./db/db.json', 'utf8', (error, file) => {
+            if (error) throw error;
+            const parsedFile = JSON.parse(file);
+            res.json(parsedFile);
+        });
     });
 
-    // app.get("./api/index", function(req, res) {
-    //     res.json(notesData);
-    // });
+    // This pushes the recent note into the .json file but not onto the page
+    app.post("/api/notes", function (req, res) {
+        let note = req.body;
+        console.log(req.body);
+        let noteId = Date.now();
+        note.id = noteId;
+        console.log(note);
 
-    app.post("/api/notes", function(req, res) {
-        notesData.push(req.body);
-            res.send(req.body);
-            console.log("note added");
+        fs.readFile('./db/db.json', 'utf8', (error, file) => {
+            if (error) throw error;
+            // parse db.json into a JSON object
+            const parsedFile = JSON.parse(file);
+            // push the new note onto the JSON object
+            parsedFile.push(note);
+            // Create new stringify of the combined file to write back to file
+            const newStringifiedFile = JSON.stringify(parsedFile);
+            // notesData = parsedFile;
+            // re-write the file as the combined file
+            fs.writeFile('./db/db.json', newStringifiedFile, 'utf8', (err) => {
+                if (err) throw err;
+                console.log("The new note was appended to the file!");
+                res.json(note);
+
+            });
         });
+    });
 
-    // app.delete("./api/notes", function(req, res) {
-        
-    // })
+    app.delete("/api/notes/:id", function (req, res) {
+        console.log(Number(req.params.id));
+        fs.readFile('./db/db.json', 'utf8', (error, file) => {
+            if (error) throw error;
+            // parse db.json into a JSON object
+            const parsedFile = JSON.parse(file);
+            const result = parsedFile.filter(note => note.id !== Number(req.params.id));
+            // notesData = result;
+            // Create new stringify of the combined file to write back to file
+            const newStringifiedFile = JSON.stringify(result);
+            // re-write the file as the combined file
+            fs.writeFile('./db/db.json', newStringifiedFile, 'utf8', (err) => {
+                if (err) throw err;
+                res.json(req.params.id);
+            });
+        });
+    });
 }
-
